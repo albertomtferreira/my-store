3c8e902c319ff5c374f1fbf640189c7ef2dd0bb7
Alberto Ferreira <albertomtferreira@gmail.com>
2023-12-15 22:25:40 +0000

    lesson 134

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App= () => {
     <Routes key={'routes'}>
       <Route path='/' element={<Navigation/>}>
         <Route index element={<Home/>}/>
-        <Route path='/shop' element={<Shop/>}/>
+        <Route path='/shop/*' element={<Shop/>}/>
         <Route path='/contact' element={<Contact/>}/>
         <Route path='/auth' element={<Authentication/>}/>
         <Route path='/checkout' element={<Checkout/>}/>

859a9ce72dabc8f83c39b5367b8231ecf64cf701
Alberto Ferreira <albertomtferreira@gmail.com>
2023-12-15 22:53:32 +0000

    lesson 135

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,26 @@
+import { useContext, useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { CategoriesContext } from '../../contexts/categories.context';
+import ProductCard from '../../components/product-card/product-card.component';
+import './category.styles.scss';
+
+const Category = () => {
+  const {category} = useParams();
+  const {categoriesMap} = useContext(CategoriesContext);
+  const [products, setProducts] = useState(categoriesMap[category]);
+
+  useEffect(()=> {
+    setProducts(categoriesMap[category]);
+  },[category, categoriesMap])
+
+  return(
+    <div key='category-container' className='category-route-container'>
+      {products &&
+        products.map((product) => <ProductCard key={product.id} product={product} /> )
+      }
+    </div>
+  )
+
+}
+
+export default Category;

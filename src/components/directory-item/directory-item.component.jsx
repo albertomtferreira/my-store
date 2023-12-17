573bb6a467447a0293047c08da9372294809d48c
Alberto Ferreira <albertomtferreira@gmail.com>
2023-12-17 16:28:22 +0000

    lesson 142

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,13 +1,17 @@
 // Homepage main body
+import { useNavigate } from 'react-router-dom';
 import {
   BackgroundImage,
   Body,
   DirectoryItemContainer,
 } from './directory-item.styles';
 
-const DirectoryItem = ({category:{title, imageUrl}})=>{
+const DirectoryItem = ({category:{title, imageUrl,route}})=>{
+  const navigate = useNavigate();
+  const onNavigateHandler = () => navigate(route);
   return[
-    <DirectoryItemContainer to={`/shop/${title}`}>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
+  
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
--- src/components/directory-item/directory-item.styles.jsx
+++ src/components/directory-item/directory-item.styles.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Link } from "react-router-dom";
+
 
 export const BackgroundImage = styled.div`
   width: 100%;
@@ -35,7 +35,7 @@ export const Body = styled.div`
   }
 `;
 
-export const DirectoryItemContainer = styled(Link)`
+export const DirectoryItemContainer = styled.div`
   min-width: 30%;
   height: 240px;
   flex: 1 1 auto;

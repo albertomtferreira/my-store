ce5bd4d6746e6a9feb673e33cd8a9639d8b7a115
Alberto Ferreira <60177437+albertomtferreira@users.noreply.github.com>
2024-01-14 11:16:17 +0000

    discard

--- src/components/payment-form/payment-form.styles.jsx
+++ src/components/payment-form/payment-form.styles.jsx
@@ -18,12 +18,10 @@ export const FormContainer = styled.form`
 	height: 100px;
 	min-width: 500px;
 	background-color: lightgrey;
-	border-radius: 10px;
-	box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
-	padding: 10px;
-	input {}
+
 
 `
 export const PaymentButton = styled(Button)`
 	margin-top: 30px;
+	margin-left: auto;
 `

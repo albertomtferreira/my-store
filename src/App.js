36159e010ab248a1b7edd97f39240b2c0f635582
Alberto Ferreira <60177437+albertomtferreira@users.noreply.github.com>
2024-01-30 20:13:03 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {useEffect, lazy, Suspense} from "react";
 
 import { checkUserSession } from './store/user/user.action';
 import Spinner from './components/spinner/spinner.component';
+import {GlobalStyle} from './global.styles';
 
 const Home = lazy(()=>import('./routes/home/home.component'));
 const Navigation = lazy(()=>import('./routes/navigation/navigation.component'));
@@ -13,14 +14,6 @@ const Contact = lazy(()=>import('./routes/contact/contact.component'));
 const Checkout = lazy(()=>import('./routes/checkout/checkout.component'));
 const Profile = lazy(()=>import('./routes/profile/profile.component'));
 
-// import Home from './routes/home/home.component';
-// import Navigation from './routes/navigation/navigation.component';
-// import Authentication from './routes/authentication/authentication.component';
-// import Shop from './routes/shop/shop.component';
-// import Contact from './routes/contact/contact.component';
-// import Checkout from './routes/checkout/checkout.component';
-// import Profile from './routes/profile/profile.component';
-
 const App= () => {
   const dispatch = useDispatch();
   useEffect(()=>{
@@ -29,16 +22,17 @@ const App= () => {
 
   return [
     <Suspense fallback={<Spinner/>}>
-      <Routes key={'routes'}>
-        <Route path='/' element={<Navigation/>}>
-          <Route index element={<Home/>}/>
-          <Route path='/shop/*' element={<Shop/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-          <Route path='/auth' element={<Authentication/>}/>
-          <Route path='/checkout' element={<Checkout/>}/>
-          <Route path='/profile' element={<Profile/>}/>
-        </Route>
-      </Routes>
+      <GlobalStyle/>
+        <Routes key={'routes'}>
+          <Route path='/' element={<Navigation/>}>
+            <Route index element={<Home/>}/>
+            <Route path='/shop/*' element={<Shop/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/auth' element={<Authentication/>}/>
+            <Route path='/checkout' element={<Checkout/>}/>
+            <Route path='/profile' element={<Profile/>}/>
+          </Route>
+        </Routes>
     </Suspense>
   ]
 }
